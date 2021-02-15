dd3994351e11c93c297beb68837e21c863c039a4
gitiss <36957738+gitiss@users.noreply.github.com>
2021-02-16 07:34:35 +0900

    update modal container

---
+++ src/Modal.js
@@ -0,0 +1,9 @@
+import './Modal.css';
+const Modal = () => {
+  return (
+    <div className='modal-background'>
+      <div className='modal-container'>ddd</div>
+    </div>
+  );
+};
+export default Modal;
