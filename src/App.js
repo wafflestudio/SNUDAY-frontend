be0fc147b214f5d0cc4d12f19c7f321893178c2a
gitiss <36957738+gitiss@users.noreply.github.com>
2021-02-18 19:17:25 +0900

    Update route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import Navigation from './Navigation';
 function App() {
   return (
     <>
       <header></header>
-      <Home />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
       <Navigation />
     </>
   );
