42d768a70410cb929e1d5ff1461d5a32b3696251
gitiss <36957738+gitiss@users.noreply.github.com>
2022-08-21 23:20:56 +0900

    Update ChannelPortal.js

--- src/channel/ChannelPortal.js
+++ src/channel/ChannelPortal.js
@@ -6,19 +6,30 @@ import Notice from './Notice';
 import { useState, useEffect } from 'react';
 import { getChannel } from 'API';
 import ErrorScreen from './ErrorScreen';
+import { useAuthContext } from '../context/AuthContext';
 const ChannelPortal = () => {
   let { channelId } = useParams();
+  const {
+    value: { isLoggedIn, isLoading },
+  } = useAuthContext();
   channelId = +channelId;
   console.log(channelId);
   const [channelData, setChannelData] = useState(null);
   const [error, setError] = useState(null);
+  console.log(isLoggedIn, isLoading);
   useEffect(() => {
-    getChannel(channelId)
-      .then((channel) => setChannelData(() => channel))
-      .catch((error) => {
-        setError(error.response?.data?.error);
-      });
-  }, []);
+    if (!isLoading)
+      getChannel(channelId)
+        .then((channel) => {
+          setChannelData(() => channel);
+          setError(null);
+        })
+        .catch((error) => {
+          setError(error.response?.data?.error);
+        });
+    // return () => setError(null);
+  }, [isLoggedIn, isLoading]);
+  if (isLoading) return <></>;
   if (error) {
     switch (error) {
       case 'private channel은 열람할 수 없습니다.':
