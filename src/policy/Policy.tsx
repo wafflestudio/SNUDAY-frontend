1ee4696c387051b38d886af1a1f821bacb4cbeed
gitiss <36957738+gitiss@users.noreply.github.com>
2022-08-25 23:53:51 +0900

    add policy menu

--- src/policy/Policy.tsx
+++ src/policy/Policy.tsx
@@ -1,5 +1,8 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Header from 'Header';
+import { useState, useEffect } from 'react';
+import privacy from './privacy.txt';
+import terms from './terms.txt';
 const getTitle = (type: string | undefined) => {
   switch (type) {
     case 'terms':
@@ -10,12 +13,37 @@ const getTitle = (type: string | undefined) => {
       return undefined;
   }
 };
+const getFile = (type: string | undefined) => {
+  switch (type) {
+    case 'terms':
+      return terms;
+    case 'privacy':
+      return privacy;
+    default:
+      return undefined;
+  }
+};
 const Policy = () => {
   let { type } = useParams();
+  const [text, setText] = useState('');
   const title = getTitle(type);
+  const file = getFile(type);
+  useEffect(() => {
+    if (getFile(type))
+      fetch(file)
+        .then((res) => res.text())
+        .then(setText)
+        .catch(console.log);
+  }, [type]);
   return (
     <>
       <Header>{title}</Header>
+      <div
+        className="main-container card policy"
+        style={{ whiteSpace: 'pre-wrap' }}
+      >
+        {text}
+      </div>
     </>
   );
 };
