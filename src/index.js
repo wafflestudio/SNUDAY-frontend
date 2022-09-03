d22663361f2532044c15a81cf379fffab8288ceb
gitiss <36957738+gitiss@users.noreply.github.com>
2022-09-03 21:10:51 +0900

    update animation & notice card skeleton

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+import './animation.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from 'context/AuthContext';
